b9a9a11852d37a3d494233ca63eb9aa587d06e82
FozleRabbi1 <fozlerabbishuvo@gmail.com>
2025-06-24 11:51:10 +0600

    dfdsfsdf

--- src/app/Modules/shared/shared.controller.ts
+++ src/app/Modules/shared/shared.controller.ts
@@ -148,7 +148,7 @@ export const findUsersBasedOnSubcategory = catchAsync(async (req, res) => {
     sendResponse(res, {
       statusCode: httpStatus.OK,
       success: true,
-      message: 'get all accepted data for each userrrrrrrrr ',
+      message: 'get all accepted data for each user ',
       data: result,
     });
   });
