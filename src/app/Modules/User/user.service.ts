09d55ea416e0333ff7c0a70d4d047e1bc597da3b
FozleRabbi1 <fozlerabbishuvo@gmail.com>
2025-07-31 13:07:13 +0600

    deleteUser udpate

--- src/app/Modules/User/user.service.ts
+++ src/app/Modules/User/user.service.ts
@@ -602,7 +602,7 @@ const deleteUser = async (userId: string) => {
 
     const userEmail = userData.email;
 
-    await User.findByIdAndUpdate({ email: userEmail }, { isDeleted: true }, { runValidators: true, new: true });
+    await User.findOneAndUpdate({ email: userEmail }, { isDeleted: true }, { runValidators: true, new: true });
     // await User.findOneAndDelete({ email: userEmail }).session(session);
 
     // await Exchange.deleteMany({
