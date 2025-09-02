b359fef7f8ec31a8b7684cd0722377aae2fd9bd2
FozleRabbi1 <fozlerabbishuvo@gmail.com>
2025-09-02 14:14:30 +0600

    remove auth form admin route

--- src/app/Modules/admin/admin.route.ts
+++ src/app/Modules/admin/admin.route.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import { addSubCategory, createCategory, deleteCategoryByAdmin, getAllCategories, getAllExchangeDatabyAdmin, removeSubCategory, showALlExchangeServiceUserMessageDataFromDBByAdmin, showALlReportMessageDataFromDBByAdmin, updateCategory } from './admin.controller';
-import { Auth } from '../../middleware/auth';
-import { User_Role } from '../User/user.constent';
+// import { Auth } from '../../middleware/auth';
+// import { User_Role } from '../User/user.constent';
 import { upload } from '../../middleware/upload';
 
 const router = express.Router();
 
-router.post('/', Auth(User_Role.admin), createCategory);
+router.post('/',
+    // Auth(User_Role.admin),
+    createCategory);
 
 router.patch(
     '/:categoryId',
@@ -21,11 +23,13 @@ router.patch('/updateCategory/:categoryId',
 
 router.delete(
     '/:categoryId',
-    Auth(User_Role.admin),
+    // Auth(User_Role.admin),
     deleteCategoryByAdmin
 );
 
-router.patch('/remove-subcategory/:categoryId', Auth(User_Role.admin), removeSubCategory);
+router.patch('/remove-subcategory/:categoryId',
+    // Auth(User_Role.admin),
+    removeSubCategory);
 
 router.get('/', getAllCategories);
 
